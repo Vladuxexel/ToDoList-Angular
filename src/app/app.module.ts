7058e51b356a160b6efefe65969fadefeabaf5f7
Vladuxexel <55832677+Vladuxexel@users.noreply.github.com>
2020-08-11 14:49:12 +0700

    Ошибка модуля

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
+import { ModalModule } from './_modal';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent
+    ListComponent,
+    ModalModule
   ],
   imports: [
     BrowserModule
