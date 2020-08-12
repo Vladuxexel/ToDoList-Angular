593be81b380ecac5d54b85841c723dc3401fc5c2
Vladuxexel <55832677+Vladuxexel@users.noreply.github.com>
2020-08-12 19:17:44 +0700

    Редактирование и удаление элементов

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ModalService } from '../_modal';
 
 interface Task{
+  id: number,
   name: string,
   description: string
 };
@@ -13,6 +14,8 @@ interface Task{
 })
 export class ListComponent implements OnInit {
 
+  onEdit: number = -1;
+  id: number = 0;
   name: string = '';
   description: string = '';
   list: Task[];
@@ -26,13 +29,41 @@ export class ListComponent implements OnInit {
 
   addNewTask(){
     if(this.name != '' && this.description != ''){
-      this.list.push({name: this.name, description: this.description});
+      if(this.onEdit != -1){
+        this.list[this.onEdit].name = this.name;
+        this.list[this.onEdit].description = this.description;
+      }
+      else{
+        this.list.push({id: this.id, name: this.name, description: this.description});
+        this.id += 1;
+      }
       this.name = '';
       this.description = '';
       this.modalService.close('modal-1');
     }
     else{
       alert('Введите данные!');
+    }
+  }
+
+  editTask(item){
+    this.onEdit = this.list.findIndex(function(task){
+      return task.id === item.id;
+    });
+    this.name = item.name;
+    this.description = item.description;
+    this.modalService.open('modal-1');
+  }
+
+  deleteTask(item){
+    const index = this.list.findIndex(function(task){
+      return task.id === item.id;
+    });
+    for(let element of this.list){
+      if(element.id === this.list[index].id){
+        this.list.splice(index, 1);
+        break;
+      }
     }
   }
 
