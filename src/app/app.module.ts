b3c0b187b98cb5c32daee6103e798e9d77a036d6
Vladuxexel <55832677+Vladuxexel@users.noreply.github.com>
2020-08-12 15:58:21 +0700

    Реализовано добавление элементов в список

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
 import { ModalModule } from './_modal';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { ModalModule } from './_modal';
   ],
   imports: [
     BrowserModule,
-    ModalModule
+    ModalModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalService } from '../_modal';
 
+interface Task{
+  name: string,
+  description: string
+};
+
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
@@ -8,14 +13,27 @@ import { ModalService } from '../_modal';
 })
 export class ListComponent implements OnInit {
 
+  name: string = '';
+  description: string = '';
+  list: Task[];
+
   constructor(public modalService: ModalService) { 
   }
 
   ngOnInit(): void {
+    this.list = [];
   }
 
-  activateDialog(){
-    alert("Dialog is active!");
+  addNewTask(){
+    if(this.name != '' && this.description != ''){
+      this.list.push({name: this.name, description: this.description});
+      this.name = '';
+      this.description = '';
+      this.modalService.close('modal-1');
+    }
+    else{
+      alert('Введите данные!');
+    }
   }
 
 }
